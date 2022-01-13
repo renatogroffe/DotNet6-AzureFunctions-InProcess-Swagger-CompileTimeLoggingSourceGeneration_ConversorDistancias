f0bacec98d9d6b0d4ede117b9f5060733e70ef52
Renato Groffe <renato.groffe@yahoo.com.br>
2022-01-13 18:12:40 -0300

    .NET 6 + Azure Functions + In-process + HTTP Trigger + Compile-time logging source generation + Swagger/Open API + Conversor de Distâncias
    .NET 6 + Azure Functions + In-process + HTTP Trigger + Compile-time logging source generation + Swagger/Open API + Conversor de Distâncias

---
+++ FunctionAppDistancias/Logging/DistanciasLogging.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace FunctionAppDistancias.Logging;
+
+public static partial class DistanciasLogging
+{
+    [LoggerMessage(EventId = 1, Level = LogLevel.Information,
+        Message = "{milhas} milhas = {km} Km")]
+    public static partial void LogConversaoDistancias(
+        this ILogger logger, double milhas, double km);
+}
